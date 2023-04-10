610a892dcc42735fa5a043832b34928dd6ac7ab4
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-04-10 11:39:55 -0300

    monday update

--- API-Express/DataBase/postQueries.js
+++ API-Express/DataBase/postQueries.js
@@ -2,28 +2,27 @@ const express = require('express');
 const conn = require('../DataBase/conect_db');
 const User = require('../ClassUser/User')
 class postQueries extends User{
-    constructor(nome,sobrenome, cidade, inscricao,Adduser, postNome, postSobreNome, postCidade, postInscricao) {
+    constructor(nome,sobrenome, cidade, inscricao) {
         super(nome, sobrenome, cidade, inscricao);
         this.router = express.Router();
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cidade = cidade;
         this.inscricao = inscricao;
-        this.router.post(`/${nome}`)
+        this.router.post(`/adduser`, this.postAddUser);
     }
     postAddUser(nome,sobrenome, cidade, inscricao, res, req){
-       nome = this.nome;
-       sobrenome = this.sobrenome;
-       cidade = this.cidade;
-       inscricao = this.inscricao;
-        const sql = `INSERT INTO cliente_tb(nome, sobrenome, cidade, inscricao) VALUES (${nome},${sobrenome},${cidade},${inscricao})`;
-        conn.query(sql,(err,result)=>{
-            if(err){
-                console.error('erro'+err.stack);
-            }else{
-                return res.json(result);
-            };
-        });
+        nome = req.body.nome;
+        sobrenome = req.body.cidade;
+        cidade = req.body.sobrenome;
+        inscricao = req.body.inscricao;
+        conn.connect().then((pool)=>{
+            const sql = `INSERT INTO cliente_tb(nome, sobrenome, cidade, inscricao) VALUES (${nome},${sobrenome},${cidade},${inscricao})`;
+            pool.query(sql).then((rows)=>{
+            })
+        })
+
     }
 
 }
+module.exports = postQueries;
--- API-Express/index.js
+++ API-Express/index.js
@@ -3,14 +3,18 @@ const ClientRoute = require('./Routes/routes');
 const conn = require('./DataBase/conect_db');
 const Teste = require('./Teste-API/test_API_conn_db');
 const Queries = require('./DataBase/getQueries')
+const postQueries = require('./DataBase/postQueries')
 const app = express();
 const port = 3000;
 
 const testedb = new Teste();
 testedb.getConnectionDb;
-const queries = new Queries();
+const getqueries = new Queries();
+const postqueries = new postQueries('Pedro', 'teste', 'parnamirim',131313);
 
-app.use('/', queries.router);
+
+app.use('/', getqueries.router);
+app.use('/', postqueries.router);
 app.get('/', (req, res)=>{
     const sql = 'SELECT * FROM cliente_tb;';
     console.dir(req.hostname);
